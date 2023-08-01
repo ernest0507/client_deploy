a7ac5a1cd56eded1e7919405124da2fb6ac2bb04
ergoog <ergoog51@gmail.com>
2023-08-02 00:04:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar  from "./components/Navbar";
+import PageNotFound  from "./components/PageNotFound";
+import MainPage  from "./components/MainPage";
+import { About } from "./components/About";
+import { Contacts } from "./components/Contacts";
+import  Login  from "./components/Login";
+import Registration from "./components/Registration";
+import CourseList from "./components/CourseList"
+import NavbarCourse from "./components/NavbarCourse"
+import NotAllowed from "./components/NotAllowed";
+import CoursePage from "./components/CoursePage";
+import NavbarCoursePage from "./components/NavbarCoursePage";
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
+  const Course = () => {
+   return sessionStorage.length > 0 ? 
+    <><NavbarCourse /> <CourseList /></>: <NotAllowed />
+ };
+
+ const CoursPage = () => {
+   return sessionStorage.length > 0 ? 
+    <><NavbarCoursePage/><CoursePage /></>: <NotAllowed />
+ };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Router>
+        <Routes>
+          <Route path="/" exact element={<><Navbar /> <MainPage /> <About /> <Contacts /></>}/>
+          <Route path="/login" exact element={<Login />}/>
+          <Route path="/registration" exact element={<Registration />}/>
+          <Route path="/resetpassword" exact element={<ResetPassword />}/>
+          <Route path="/course" exact element={<Course />}/>
+          <Route path="/course/:id" exact element={<CoursPage/>}/>
+          <Route path="*" exact element={<PageNotFound />}/>
+        </Routes>
+    </Router>
+  </>
   );
 }
 
---
+++ src/components/CoursePage.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+export default function CoursePage () { 
+  let { id }  = useParams()
+  const [link, setLink] = useState("")
+    
+  useEffect(() => {
+      axios.get(`http://localhost:8800/link/byId/${id}`).then((response) => {
+          setLink(response.data)
+      })
+  }, [])
+
+  return (
+    <>
+    <div className="pt-20">
+      <iframe title=" " className="w-full" height={window.innerHeight - 80} src={link.link}/>
+    </div>
+    </>
+  )
+}
